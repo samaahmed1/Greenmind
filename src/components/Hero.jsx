a70ea00c4b1006f63c7e0f9374a89196470c1b6c
samaahmed1 <sama34777@gmail.com>
2025-03-10 22:14:47 +0200

    Add: Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="container pt-10 md:pt-0 px-5 mx-auto bg-[#C1DCDC] rounded-xl flex justify-center md:justify-between items-center flex-wrap gap-7 md:gap-0">
+        <div className="col flex flex-col gap-7">
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Buy your <br /> dream plants
+          </h1>
+          <div className="statistics flex gap-7">
+            <div className="col">
+              <p className="font-semibold text-3xl">50+</p>
+              <span>Plant Species</span>
+            </div>
+            <span className="border-black border-r"></span>
+            <div className="col">
+              <p className="font-semibold text-3xl">100+</p>
+              <span>Customers</span>
+            </div>
+          </div>
+            <div className="search border bg-white flex justify-between items-center p-2 rounded-md gap-4">
+              <input className="border-none outline-none" type="text" name="search" id="search" placeholder="What are you looking for?" />
+              <i className="fa-solid fa-magnifying-glass bg-[#C1DCDC] rounded-md p-2"></i>
+            </div>
+        </div>
+        <div className="col relative">
+          <img className="absolute top-0 right-0" src="/src/assets/icons/Arrow.webp" alt="Arrow" />
+          <img src="/src/assets/images/hero.webp" alt="hero" />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -6,14 +6,14 @@ export default function Header() {
     setOpenNav(!openNav);
   }
   return (
-    <header className=" text-black py-5 sticky top-0 bg-white">
+    <header className=" text-black py-5 sticky top-0 bg-white z-10">
       <div className="container px-5 mx-auto flex items-center justify-between gap-0 md:gap-10">
         <div className="logo text-2xl font-semibold font-sans">
           <Link to={"/"}>GREENMIND</Link>
         </div>
         <i onClick={toggleNav} className="fa-solid fa-bars text-2xl text-black block md:hidden cursor-pointer"></i>
         <nav
-          className={`absolute top-16 left-0 md:static flex gap-4 md:flex flex-col md:flex-row justify-center md:justify-between md:items-center w-full p-5 bg-white md:p-0 ${
+          className={`absolute top-16 left-0 md:static mx-auto flex gap-4 md:flex flex-col md:flex-row justify-center md:justify-between md:items-center w-full p-5 rounded-xl bg-white border-2 border-[#C1DCDC] md:border-none md:p-0 ${
             openNav ? "block" : "hidden"
           }`}
         >
@@ -55,7 +55,7 @@ export default function Header() {
           <div className="nav-icon flex items-center justify-center gap-5">
             <div className="cart flex gap-5">
               <i className="fa-solid fa-cart-shopping"></i>
-              <span className="border-gray-500 border-r-2"></span>
+              <span className="border-black border-r"></span>
             </div>
             <i className="fa-solid fa-user"></i>
           </div>
