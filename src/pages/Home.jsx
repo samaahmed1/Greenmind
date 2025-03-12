af4c2e9de9d07ba7c2f285722e2e68aa9e8315e0
samaahmed1 <sama34777@gmail.com>
2025-03-12 22:10:03 +0200

    feat: add scroll animations to Home page sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,46 @@
 import Hero from "../components/Hero";
+import { motion } from "framer-motion";
 import AboutUs from "../components/AboutUs";
 import Categories from "../components/Categories";
 import BestSelling from "../components/BestSelling";
+import { useInView } from "react-intersection-observer";
 import CustomerReviews from "../components/CustomerReviews";
+
+const SectionWrapper = ({ children }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <motion.div ref={ref} initial={{ opacity: 0, y: 70 }} animate={inView ? { opacity: 1, y: 0 } : {}} transition={{ duration: 0.5 }}>
+      {children}
+    </motion.div>
+  );
+};
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <BestSelling />
-      <AboutUs />
-      <Categories />
-      <CustomerReviews />
+      <SectionWrapper>
+        <Hero />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <BestSelling />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <AboutUs />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <Categories />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <CustomerReviews />
+      </SectionWrapper>
     </>
   );
 }
