25d1e825d78e1f41db7308b7bcfa49d7a5d5d40d
samaahmed1 <sama34777@gmail.com>
2025-03-12 21:28:14 +0200

    Add transition animations using framer-motion

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,47 @@ import { createRoot } from "react-dom/client";
 import Footer from "./components/layouts/Footer.jsx";
 import Header from "./components/layouts/Header.jsx";
 import PlantsGallery from "./pages/PlantsGallery.jsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 import ScrollToTopButton from "./components/ScrollToTop.jsx";
+import { AnimatePresence, motion } from "framer-motion";
+
+const AnimatedRoutes = () => {
+  const location = useLocation(); 
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }} 
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }} 
+            >
+              <App />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/PlantsGallery"
+          element={
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+              <PlantsGallery />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/PlantsGallery" element={<PlantsGallery />} />
-      </Routes>
+      <AnimatedRoutes />
       <ScrollToTopButton />
       <Footer />
     </BrowserRouter>
